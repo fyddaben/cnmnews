f94f04d23e0616ea9896d1b9bd8031d6d5a87c9a
fanyongdong <fanyongdong@xiaomi.com>
2017-01-08 02:32:13 +0800

    update

--- app/javascripts/app.vue
+++ app/javascripts/app.vue
@@ -3,9 +3,29 @@
     <router-view></router-view>
   </div>
 </template>
-
-<style lang='sass'>
-  @import "../commons/reset.scss";
+<style>
+  html {
+    text-size-adjust: 100%;
+  }
+  html, body {
+    user-select: none;
+  }
+  body {
+    line-height: 1.6;
+    font-family: -apple-system-font, "Helvetica Neue",Helvetica,Arial,sans-serif;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    text-decoration: none;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+  a, img {
+    border: 0;
+    touch-callout: none; /* 禁止长按链接与图片弹出菜单 */
+  }
 </style>
 <script >
 export default {
